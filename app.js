9afbf52c51f365f60cb1c674c81b4cc38559826e
shaharia <156238113+tamimfar@users.noreply.github.com>
2025-01-04 13:49:51 +0600

    df

---
+++ app.js
@@ -0,0 +1,26 @@
+let acc = document.getElementsByClassName("accordion")
+let i;
+for(i = 0 ;i < acc.length; i++){
+   acc[i].addEventListener("click",function(){
+      let panel = this.nextElementSibling;
+       if(panel.style.maxHeight){
+         panel.style.maxHeight = null;
+       }
+       else{
+         panel.style.maxHeight = panel.scrollHeight + "px";
+          
+         }
+      
+     
+      
+   })
+}
+console.log(acc);
+
+
+
+
+
+
+
+
